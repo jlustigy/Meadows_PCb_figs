88aa6e895dc5f72711b40b6062d345603d07cf8d
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2017-01-13 18:03:47 -0800

    random small update

--- figure_scripts/fig07_atm_dessCO2/fig07.py
+++ figure_scripts/fig07_atm_dessCO2/fig07.py
@@ -11,8 +11,19 @@ def make_fig():
     from utils import fig_params
     import utils.atm as atm
     import os
+    sys.path.append("../../")
+    from utils import fig_params
 
-    
+    # Params specific to this plot
+    savetag = "fig07"
+    fname = "1barGao_final.atm"
+
+    # Read in atm files
+    atmpath = os.path.join(os.path.dirname(__file__),"model_outputs/", fname)
+    data = np.genfromtxt(atmpath, skip_header=2)
+
+    print data.shape
+    # Feed to plotting function
 
     return
 #########################################

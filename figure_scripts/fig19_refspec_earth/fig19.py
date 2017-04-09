e472c08f559b5aea4bd900e20c1d85dccbfaab9a
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2017-04-08 18:56:15 -0700

    molecule labels

--- figure_scripts/fig19_refspec_earth/fig19.py
+++ figure_scripts/fig19_refspec_earth/fig19.py
@@ -20,6 +20,12 @@ def make_fig():
     labels1 = ["O2", "O3", "O4", "CO", "CO2", "H2O"]
     labels2 = ["O2", "O3", "O4", "CO", "CO2"]
 
+    moleloc = {"O$_2$" : [(0.15, 0.01), (0.63, 0.04), (0.69, 0.019), (0.76, 0.0), (1.27, .013)],
+               "O$_3$" : [(0.25, 0.01), (0.62, 0.065)],
+               "CO$_2$" : [(0.88, 0.05), (1.05, 0.045), (1.21, .046), (1.62, 0.03), (1.75, 0.051), (2.01, 0.03), (2.45, 0.055)],
+               "CO" : [(0.15, 0.02), (1.2, 0.054), (1.58, 0.047), (2.35, 0.03)]
+          }
+
     # import basic dependencies
     import numpy as np
     import os
@@ -45,7 +51,7 @@ def make_fig():
 
     # Make reflectance plot
     spc.plot_rad(fname_new, savetag=savetag, lammin=lammin, lammax=lammax, ylim=ylim,
-                     title=title1, labels=None)
+                     title=title1, labels=None, moleloc=moleloc)
 
     return
 #########################################
